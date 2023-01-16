ac94128737dc2851a0b1b5391cbf237a1aed752a
Junior Schons <100052925+Junior-Schons@users.noreply.github.com>
2023-01-15 22:51:26 -0300

    att
    att

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,31 @@ import ComParametro from "./components/basicos/ComParametro";
 import Primeiro from "./components/basicos/primeiro";
 import Fragmento from "./components/basicos/Fragmento";
 import Aleatorio from "./components/basicos/Aleatorio";
+import Card from "./components/layout/Card";
 
-export default (props)=>{
+export default () => {
     return (
         <div id='app'>
             <h1>Fundamentos React</h1>
-            <Aleatorio min={1} max={60}/>
-            <Fragmento />
-            <ComParametro
-                titulo='situação do aluno'
-                aluno='Pedro'
-                nota={9.3}
-            />
-            <Primeiro />
+            <Card titulo='Desafio Aleatório'>
+                <Aleatorio min={1} max={60} />
+            </Card>
+
+            <Card titulo='Fragmento'>
+                <Fragmento />
+            </Card>
+
+            <Card titulo='Com Parametro'>
+                <ComParametro
+                    titulo='situação do aluno'
+                    aluno='Pedro'
+                    nota={9.3}
+                />
+            </Card>
+
+            <Card titulo='Primeiro Componente'>
+                <Primeiro />
+            </Card>
         </div>
     );
 }
